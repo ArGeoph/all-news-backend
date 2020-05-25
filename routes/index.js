6973415b2eb6e43ac658a5b6d10001f3653c77c3
argeoph <arnasay@gmail.com>
2020-05-25 09:21:02 -0600

    Add HTTPS support. Add helmet package to enforce all traffic use HTTPS

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const { newsUrl } = require('../utils/urls');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express app' });
 });
 
 
