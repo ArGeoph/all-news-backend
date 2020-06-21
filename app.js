8ee9351d4c0d6488d230f78e994f106fa36474c5
argeoph <arnasay@gmail.com>
2020-06-21 16:41:06 -0600

    Add express-validator library. Refactor topHeadlines route. Add basic sanitazation for topHeadlines route

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const topHeadlinesRouter = require('./routes/topHeadlines');
 
 const app = express();
 
-// ddd CORS support
+// Add CORS support
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
--- routes/topHeadlines.js
+++ routes/topHeadlines.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 const fetchNews = require('../utils/fetchHelpers').fetchDataFromNewsApi;
 const { topHeadlinesUrl } = require('../utils/urls');
+const { query } = require('express-validator')
 
 /* Get top headlines for provided country code
 * Expected parameters: country (country code, such as ca, us, fr, or ru)
 *
 * */
-router.get('/', async function f(req, res) {
-  const response = await fetchNews(topHeadlinesUrl, req.query);
-  res.json(response);
+router.get('/', query('country').trim().escape(), async function f(req, res) {
+  // Extract only required parameters from request query
+  const { country } = req.query;
+
+  const response = await fetchNews(topHeadlinesUrl, { country });
+  return res.json(response);
 });
 
 module.exports = router;
